0e103a0d4a5ed5630b166ecd9279d41dcf613bfc
lewiswon <lewis.won@gmail.com>
2024-12-10 15:12:48 +0800

    cleaned up function in go

--- wiki.go
+++ wiki.go
@@ -1,26 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"github.com/elastic/go-elasticsearch/v8"
 	"log"
 	"os"
 )
 
 func main() {
-	cert, _ := os.ReadFile("./http_ca.crt")
-	cfg := elasticsearch.Config{
-		Addresses: []string{
-			"https://localhost:9200",
-		},
-		Username: "elastic",
-		Password: "-gN-NuCy+qmr5IowL_X_",
-		CACert:   cert,
-	}
-	es, err := elasticsearch.NewClient(cfg)
+	es, err := GetElasticsearchClient()
 	if err != nil {
 		log.Fatalf("Error getting response: %s", err)
 	}
 	log.Println(elasticsearch.Version)
 	log.Println(es.Info())
 
+}
+
+func GetElasticsearchClient() (*elasticsearch.TypedClient, error) {
+	username := os.Getenv("ELASTIC_USERNAME")
+	password := os.Getenv("ELASTIC_PASSWORD")
+	cert, _ := os.ReadFile("./http_ca.crt")
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"https://localhost:9200",
+		},
+		Username: username,
+		Password: password,
+		CACert:   cert,
+	}
+	es, err := elasticsearch.NewTypedClient(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect: %w", err)
+	}
+
+	return es, nil
 }

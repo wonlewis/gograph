48cd0562d75987f83e557977fc730e9c46dd24c9
lewiswon <lewis.won@gmail.com>
2025-01-05 11:16:28 +0800

    implementing unidirectional

--- dao/ElasticGraphQuery.go
+++ dao/ElasticGraphQuery.go
@@ -11,9 +11,8 @@ import (
 )
 
 // Env Credit on using elasticsearch client as a struct: https://github.com/gin-gonic/gin/issues/932
-type Env struct {
-	Db      *elasticsearch.Client
-	DbTyped *elasticsearch.TypedClient
+type ElasticGraphQueryDAO struct {
+	Db *elasticsearch.TypedClient
 }
 
 type IGraphQueryDAO interface {
@@ -22,7 +21,7 @@ type IGraphQueryDAO interface {
 	NodeAttributeQuery(queryParam models.NodeAttributeQueryParam) []interface{}
 }
 
-func (e *Env) BidirectionalQuery(nodeQuery models.NodeQueryModel) (result models.QueryResultModel) {
+func (e *ElasticGraphQueryDAO) BidirectionalQuery(nodeQuery models.NodeQueryModel) (result models.QueryResultModel) {
 	var boolQuery *types.BoolQuery
 	if nodeQuery.HopLeft == 0 {
 		return models.QueryResultModel{
@@ -44,7 +43,7 @@ func (e *Env) BidirectionalQuery(nodeQuery models.NodeQueryModel) (result models
 	var size *int
 	size = new(int)
 	*size = 1
-	res, err := e.DbTyped.Search().
+	res, err := e.Db.Search().
 		Index(nodeQuery.Datasource).
 		Request(&search.Request{
 			Query: &types.Query{
@@ -60,8 +59,113 @@ func (e *Env) BidirectionalQuery(nodeQuery models.NodeQueryModel) (result models
 	var r map[string]interface{}
 	err = json.NewDecoder(res.Body).Decode(&r)
 	if err != nil {
-		log.Println("Error decoding response: %s", err)
+		log.Printf("Error decoding response: %s\n", err)
 		return
+
+	}
+	aggregations := r["aggregations"].(map[string]interface{})
+	fromObjects := aggregations[nodeQuery.FromField].(map[string]interface{})["buckets"].([]interface{})
+	toObjects := aggregations[nodeQuery.ToField].(map[string]interface{})["buckets"].([]interface{})
+	allObjects := append(fromObjects, toObjects...)
+	var graphNodes []models.NodeModel
+	for _, v := range allObjects {
+		graphNodes = append(graphNodes, models.NodeModel{
+			FieldName:  nodeQuery.CommonFieldName,
+			FieldValue: v.(map[string]interface{})["key"].(string),
+			Datasource: nodeQuery.Datasource,
+		})
+	}
+	if len(graphNodes) > 0 {
+		graphNodes = append(graphNodes, models.NodeModel{
+			FieldName:  nodeQuery.CommonFieldName,
+			FieldValue: nodeQuery.Value,
+			Datasource: nodeQuery.Datasource,
+		})
+	}
+	var graphQueries []models.NodeQueryModel
+	for _, v := range allObjects {
+		if nodeQuery.HopLeft > 1 {
+			graphQueries = append(graphQueries, models.NodeQueryModel{
+				FromField:          nodeQuery.FromField,
+				ToField:            nodeQuery.ToField,
+				Value:              v.(map[string]interface{})["key"].(string),
+				Constraints:        nodeQuery.Constraints,
+				Datasource:         nodeQuery.Datasource,
+				NumberOfNeighbours: nodeQuery.NumberOfNeighbours,
+				QuerySize:          nodeQuery.QuerySize,
+				HopLeft:            nodeQuery.HopLeft - 1,
+				CommonFieldName:    nodeQuery.CommonFieldName,
+				Reverse:            false,
+			})
+		}
+	}
+	var graphEdges []models.EdgeModel
+	for _, v := range fromObjects {
+		graphEdges = append(graphEdges, models.EdgeModel{
+			ToFieldName:    nodeQuery.CommonFieldName,
+			ToFieldValue:   nodeQuery.Value,
+			FromFieldName:  nodeQuery.CommonFieldName,
+			FromFieldValue: v.(map[string]interface{})["key"].(string),
+			Datasource:     nodeQuery.Datasource,
+			Frequency:      int(v.(map[string]interface{})["doc_count"].(float64)),
+		})
+	}
+	for _, v := range toObjects {
+		graphEdges = append(graphEdges, models.EdgeModel{
+			ToFieldName:    nodeQuery.CommonFieldName,
+			ToFieldValue:   v.(map[string]interface{})["key"].(string),
+			FromFieldName:  nodeQuery.CommonFieldName,
+			FromFieldValue: nodeQuery.Value,
+			Datasource:     nodeQuery.Datasource,
+			Frequency:      int(v.(map[string]interface{})["doc_count"].(float64)),
+		})
+	}
+	return models.QueryResultModel{
+		Nodes:       graphNodes,
+		Edges:       graphEdges,
+		NodeQueries: graphQueries,
+	}
+}
+
+func (e *ElasticGraphQueryDAO) UnidirectionalQuery(nodeQuery models.NodeQueryModel) (result models.QueryResultModel) {
+	var boolQuery *types.BoolQuery
+	if nodeQuery.HopLeft == 0 {
+		return models.QueryResultModel{
+			Nodes:       make([]models.NodeModel, 0),
+			Edges:       make([]models.EdgeModel, 0),
+			NodeQueries: make([]models.NodeQueryModel, 0),
+		}
+	}
+	boolQueryArray := BoolQueryForUnidirectional(nodeQuery)
+	if len(nodeQuery.Constraints) > 0 {
+		for _, queryConstraint := range nodeQuery.Constraints {
+			boolQueryArray = append(boolQueryArray, queryConstraint)
+		}
+	}
+	boolQuery.Filter = boolQueryArray
+	aggregationQueryAsList := AggregationUnidirection(nodeQuery)
+	var size *int
+	size = new(int)
+	*size = nodeQuery.QuerySize
+	res, err := e.Db.Search().
+		Index(nodeQuery.Datasource).
+		Request(&search.Request{
+			Query: &types.Query{
+				Bool: boolQuery,
+			},
+			Size:         size,
+			Aggregations: aggregationQueryAsList,
+		}).Do(context.Background())
+	if err != nil {
+		log.Println("Error getting response: %s", err)
+		return
+	}
+	var r map[string]interface{}
+	err = json.NewDecoder(res.Body).Decode(&r)
+	if err != nil {
+		log.Printf("Error decoding response: %s\n", err)
+		return
+
 	}
 	aggregations := r["aggregations"].(map[string]interface{})
 	fromObjects := aggregations[nodeQuery.FromField].(map[string]interface{})["buckets"].([]interface{})
@@ -132,12 +236,12 @@ func BoolQueryForBidirectional(value string, fromField string, toField string) *
 	*minimumShouldMatch = 1
 	boolQuery := types.NewBoolQuery()
 	boolQuery.Should = []types.Query{
-		types.Query{
+		{
 			Match: map[string]types.MatchQuery{
 				fromField: {Query: value},
 			},
 		},
-		types.Query{
+		{
 			Match: map[string]types.MatchQuery{
 				toField: {Query: value},
 			},
@@ -145,6 +249,27 @@ func BoolQueryForBidirectional(value string, fromField string, toField string) *
 	}
 	boolQuery.MinimumShouldMatch = minimumShouldMatch
 	return boolQuery
+}
+
+func BoolQueryForUnidirectional(nodeQuery models.NodeQueryModel) []types.Query {
+	minimumShouldMatch := new(types.MinimumShouldMatch)
+	*minimumShouldMatch = 1
+	matchQuery := map[string]types.MatchQuery{}
+	if nodeQuery.Reverse == true {
+		matchQuery[nodeQuery.ToField] = types.MatchQuery{
+			Query: nodeQuery.Value,
+		}
+	} else {
+		matchQuery[nodeQuery.FromField] = types.MatchQuery{
+			Query: nodeQuery.Value,
+		}
+	}
+	boolQueryArray := []types.Query{
+		{
+			Match: matchQuery,
+		},
+	}
+	return boolQueryArray
 }
 
 func AggregationTerms(value string, field string, numberOfNeighbours int) map[string]types.Aggregations {
@@ -157,5 +282,26 @@ func AggregationTerms(value string, field string, numberOfNeighbours int) map[st
 		},
 	}
 	aggregations[field] = aggregationQuery
+	return aggregations
+}
+
+func AggregationUnidirection(nodeQuery models.NodeQueryModel) map[string]types.Aggregations {
+	termsAggregation := types.TermsAggregation{
+		Size: &nodeQuery.NumberOfNeighbours,
+	}
+	if nodeQuery.Reverse == true {
+		termsAggregation.Field = &nodeQuery.FromField
+	} else {
+		termsAggregation.Field = &nodeQuery.ToField
+	}
+	aggregations := make(map[string]types.Aggregations)
+	aggregationQuery := types.Aggregations{
+		Terms: &termsAggregation,
+	}
+	if nodeQuery.Reverse == true {
+		aggregations[nodeQuery.FromField] = aggregationQuery
+	} else {
+		aggregations[nodeQuery.ToField] = aggregationQuery
+	}
 	return aggregations
 }

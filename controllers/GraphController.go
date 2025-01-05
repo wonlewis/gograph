462f84f5cd2db7732d62cf7edbc82227362478fb
lewiswon <lewis.won@gmail.com>
2025-01-05 16:29:09 +0800

    implementeed in full

--- controllers/GraphController.go
+++ controllers/GraphController.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"graph/models"
 	"graph/services"
+	"log"
+	"net/http"
 )
 
 type IGraphController interface {
@@ -16,9 +18,27 @@ type GraphController struct {
 }
 
 func (graphController *GraphController) GraphQuery(c *gin.Context) {
-	return graphController.GraphService.GraphSearch(graphParam)
+	var request models.GraphParam
+	if err := c.BindJSON(&request); err != nil {
+		log.Println("Invalid json request: %s", err)
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	graphData, validationResponse := graphController.GraphService.GraphSearch(request)
+	if validationResponse.Validity == false {
+		c.IndentedJSON(http.StatusBadRequest, validationResponse)
+	} else {
+		c.IndentedJSON(http.StatusOK, graphData)
+	}
 }
 
 func (graphController *GraphController) NodeAttributeSearch(c *gin.Context) {
-	return graphController.GraphService.NodeAttributeSearch(graphParam)
+	var request models.NodeAttributeQueryParam
+	if err := c.BindJSON(&request); err != nil {
+		log.Println("Invalid json request: %s", err)
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	result := graphController.GraphService.NodeAttributeSearch(request)
+	c.IndentedJSON(http.StatusOK, result)
 }
